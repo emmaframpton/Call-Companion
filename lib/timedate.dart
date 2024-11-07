f0ae6702aceac268d5bac620b6a77ff8d6d3320c
g33ya <gscozzaro2004@gmail.com>
2024-11-07 13:16:18 -0500

    basic file setup

---
+++ lib/timedate.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Call Companion',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Months(title: "Months"),
+    );
+  }
+}
